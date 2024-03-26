29d8d04afc2737c93982699817867822fe7819c0
Lucien Donnarieix <lucien.donnarieix@hesias.fr>
2024-03-27 00:01:51 +0100

    Ajout d'une fleche permettant de scroll en haut de la page

--- scripts/index.js
+++ scripts/index.js
@@ -36,4 +36,21 @@ document.addEventListener('DOMContentLoaded', function() {
             window.location.href = location;
         });
     });
+
+    // Fleche
+    var fleche = document.getElementById('fleche');
+    fleche.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+    document.addEventListener('scroll', function() {
+        if (window.scrollY > 100){
+            fleche.classList.add('active');
+        } else {
+            fleche.classList.remove('active');
+        }
+    });
 });
