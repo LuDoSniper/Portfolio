cdee6209cda217fccc98f33fa737c8286ac0c594
Lucien Donnarieix <lucien.donnarieix@hesias.fr>
2024-05-06 15:15:57 +0200

    Refonte de la barre de navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Scroll
-    var balisesA = document.querySelectorAll('header nav a');
+    // Scroll click nav
+    var balisesA = document.querySelectorAll('header nav a, #nav nav a');
 
     balisesA.forEach(function(baliseA) {
         baliseA.addEventListener('click', function(event) {
@@ -19,6 +19,44 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    // Toggle-nav
+    var toggle_nav_overlay = document.querySelector('#toggle-nav .overlay');
+    var toggle_nav = document.getElementById('toggle-nav');
+    var nav = document.getElementById('nav');
+    toggle_nav_overlay.addEventListener('click', function() {
+        if (toggle_nav.classList.contains('up')){
+            toggle_nav.classList.remove('up');
+            toggle_nav.classList.add('down');
+        } else if (toggle_nav.classList.contains('down')){
+            toggle_nav.classList.remove('down');
+            toggle_nav.classList.add('up');
+        }
+
+        var left = document.querySelector('#toggle-nav .arrow.left');
+        var right = document.querySelector('#toggle-nav .arrow.right');
+
+        if (left.classList.contains('up')){
+            left.classList.remove('up');
+            right.classList.remove('up');
+            left.classList.add('down');
+            right.classList.add('down');
+        } else if (left.classList.contains('down')){
+            left.classList.remove('down');
+            right.classList.remove('down');
+            left.classList.add('up');
+            right.classList.add('up');
+        }
+
+
+        if (nav.classList.contains('hide')){
+            nav.classList.add('show');
+            nav.classList.remove('hide');
+        } else if (nav.classList.contains('show')){
+            nav.classList.add('hide');
+            nav.classList.remove('show');
+        }
+    });
+
     // Fleche
     var fleche = document.getElementById('fleche');
     fleche.addEventListener('click', function() {
@@ -26,13 +64,36 @@ document.addEventListener('DOMContentLoaded', function() {
             top: 0,
             behavior: "smooth"
         });
+
+        toggle_nav.classList.remove('active');
+        toggle_nav.classList.remove('down');
+        toggle_nav.classList.add('up');
+
+        nav.classList.remove('show');
+        nav.classList.add('hide');
     });
 
+    // Window ScrollY
     document.addEventListener('scroll', function() {
+        // Toggle-nav
         if (window.scrollY > 100){
+            // Toggle-nav
+            toggle_nav.classList.add('active');
+
+            // Fleche
             fleche.classList.add('active');
         } else {
+            // Toggle-nav
+            toggle_nav.classList.remove('active');
+            toggle_nav.classList.add('up');
+            toggle_nav.classList.remove('down');
+
+            // Fleche
             fleche.classList.remove('active');
+
+            // Nav
+            nav.classList.remove('show');
+            nav.classList.add('hide');
         }
     });
 });
